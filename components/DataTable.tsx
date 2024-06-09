70f66b503a453a63bc1e962fa25df3ed05b01532
LilAliTech <lilalix20@gmail.com>
2024-06-09 15:44:46 +0100

    Search input functionality

--- components/DataTable.tsx
+++ components/DataTable.tsx
@@ -1,4 +1,5 @@
-import React from 'react'
+'use client'
+import React, { useState } from 'react'
 import SearchInput from '@/components/SearchInput'
 import {
   Table,
@@ -13,37 +14,56 @@ import {
 
 
 function DataTable({countries}:any) {
+  
+  const [keyword, setKeyword] = useState('');
+  
+  const filteredCountries = countries.filter((country:any) => 
+    country.name.common.toLowerCase().includes(keyword) ||
+    country.region.toLowerCase().includes(keyword)
+    // country.subregion.common.toLowerCase().includes(keyword)
+  
+  );
+
+  const onInputChange = (e:any) => {
+    e.preventDefault();
+
+    setKeyword(e.target.value.toLowerCase());
+  }
+
   return (
     <div className='w-full p-4 shadow-lg md:rounded-2xl relative top-[-4rem] bg-[#1B1D1F] border-solid border-[1px] border-[#282b30]'>
       <div className='flex flex-1 p-4'>
         <h1 className='text-[#6C727F] flex flex-1 font-medium text-[16px] items-center'>Found {countries.length} countries</h1>
-        <SearchInput />
+        <SearchInput onChange={onInputChange} />
       </div>
-      <section>
-        <Table className=''>
-          <TableCaption>A list of your recent invoices.</TableCaption>
-          <TableHeader>
-            <TableRow>
-              <TableHead className="w-[100px]">Flag</TableHead>
-              <TableHead>Name</TableHead>
-              <TableHead>Population</TableHead>
-              <TableHead>Area(km^2)</TableHead>
-              <TableHead>Region</TableHead>
-            </TableRow>
-          </TableHeader>
-          {countries.map((country: any) => 
-            <TableBody key={country.name.common} className=''>
+      <section className='lg:flex mx-6 items-start'>
+        <section>
+          Sort by
+        </section>
+        <section className='md:ml-auto mt-6 lg:w-2/3'>
+          <Table className='min-w-[600px] w-full table-fixed text-sm text-left rtl:text-right text-gray-500'>
+            <TableHeader className=''>
               <TableRow>
-                <TableCell className="text-5xl">{country.flag}</TableCell>
-                <TableCell>{country.name.common}</TableCell>
-                <TableCell>{country.population.toLocaleString()}</TableCell>
-                <TableCell>{country.area.toLocaleString()}</TableCell>
-                <TableCell>{country.region}</TableCell>
+                <TableHead className="w-[100px] text-center">Flag</TableHead>
+                <TableHead className='text-center'>Name</TableHead>
+                <TableHead className='text-center'>Population</TableHead>
+                <TableHead className='text-center'>Area(km^2)</TableHead>
+                <TableHead className='text-center'>Region</TableHead>
               </TableRow>
-            </TableBody>
-          )}
-        </Table>
-
+            </TableHeader>
+            {filteredCountries.map((country: any) => 
+              <TableBody key={country.name.common} className='text-center'>
+                <TableRow>
+                  <TableCell className="text-5xl">{country.flag}</TableCell>
+                  <TableCell className=''>{country.name.common}</TableCell>
+                  <TableCell>{country.population.toLocaleString()}</TableCell>
+                  <TableCell>{country.area.toLocaleString()}</TableCell>
+                  <TableCell>{country.region}</TableCell>
+                </TableRow>
+              </TableBody>
+            )}
+          </Table>
+        </section>
       </section>
     </div>
   )
